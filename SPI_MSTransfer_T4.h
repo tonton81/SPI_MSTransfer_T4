94a498c5b0034cfd642c2a8b1e328b8555155779
Antonio Brewer <yodabox64@hotmail.com>
2022-03-07 00:02:12 -0500

    Update SPI_MSTransfer_T4.h

--- SPI_MSTransfer_T4.h
+++ SPI_MSTransfer_T4.h
@@ -6,6 +6,27 @@
 #include <functional>
 #include <SPI.h>
 
+#define SLAVE_CR spiAddr[4]
+#define SLAVE_FCR spiAddr[22]
+#define SLAVE_FSR spiAddr[23]
+#define SLAVE_IER spiAddr[6]
+#define SLAVE_CFGR0 spiAddr[8]
+#define SLAVE_CFGR1 spiAddr[9]
+#define SLAVE_TDR spiAddr[25]
+#define SLAVE_RDR spiAddr[29]
+#define SLAVE_SR spiAddr[5]
+#define SLAVE_TCR_REFRESH spiAddr[24] = (2UL << 27) | LPSPI_TCR_FRAMESZ(16 - 1)
+#define SLAVE_PORT_ADDR volatile uint32_t *spiAddr = &(*(volatile uint32_t*)(0x40394000 + (0x4000 * _portnum)))
+
+#define SPI_WAIT_STATE \
+    while ( !(SLAVE_SR & (1UL << 9)) ) { /* FCF: Frame Complete Flag, set when PCS deasserts */ \
+      if ( !(SLAVE_FSR & 0x1F0000) ) continue; /* wait for received data */ \
+      if ( (SLAVE_SR & (1UL << 8)) ) { /* WCF set */
+#define SPI_ENDWAIT_STATE \
+      } \
+    }
+
+
 #define SPI_MST_QUEUE_SLOTS 8
 #define SPI_MST_DATA_BUFFER_MAX 250
 
@@ -30,9 +51,6 @@ class SPI_MSTransfer_T4_Base {
     virtual void SPI_MSTransfer_SLAVE_ISR();
 };
 
-//static SPI_MSTransfer_T4_Base* _LPSPI1 = nullptr;
-//static SPI_MSTransfer_T4_Base* _LPSPI2 = nullptr;
-//static SPI_MSTransfer_T4_Base* _LPSPI3 = nullptr;
 static SPI_MSTransfer_T4_Base* _LPSPI4 = nullptr;
 
 Circular_Buffer<uint16_t, (uint32_t)pow(2, ceil(log(SPI_MST_QUEUE_SLOTS) / log(2))), SPI_MST_DATA_BUFFER_MAX> mstqueue;
@@ -46,6 +64,7 @@ SPI_MSTransfer_T4_CLASS class SPI_MSTransfer_T4 : public SPI_MSTransfer_T4_Base 
     uint32_t events();
 
   private:
+    SLAVE_PORT_ADDR;
     void SPI_MSTransfer_SLAVE_ISR();
     int _portnum = 0;
     uint32_t nvic_irq = 0;
